324723130b36bc78cd0ad251a8da3cc22f19b289
Sebastian <sebastianmaludzinski@icloud.com>
2021-10-05 18:16:41 -0700

    Initial Commit

---
+++ FetchDataUinitTests/FetchDataUinitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDataUinitTestsApp.swift
+//  FetchDataUinitTests
+//
+//  Created by Sebastian Maludziński on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FetchDataUinitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
